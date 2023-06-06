7975f103015912f04f45fa295562ada257a8476d
Csuarez8 <csuarez.edu8@gmail.com>
2023-06-06 17:24:33 -0400

    Re-wrote script

---
+++ js/index.js
@@ -0,0 +1,44 @@
+console.log("Hey");
+
+const options= ["rock", "paper", "scissors"];
+
+function getComputerChoice (){
+    const choice = options[Math.floor(Math.random()* options.length)]; //Don't understand completely//
+    console.log(choice);
+    return choice;
+}
+
+function checkWinner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "Tie";
+    } else if (
+        (playerSelection == "rock" && computerSelection == "scissors")||
+        (playerSelection == "scissors" && computerSelection == "paper")||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ){
+        return "Player";
+    } else {
+        return "Computer";
+    }
+
+}
+
+
+
+function playRound (playerSelection, computerSelection){
+    const result = checkWinner(playerSelection, computerSelection);
+    if(result =="Tie"){
+        return "It's a tie"
+    }
+    else if (result =="Player"){
+        return `You win ${playerSelection} beats ${computerSelection}`;
+    }
+    else{
+        return `You lost ${computerSelection} beats ${playerSelection}`;
+    }
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
